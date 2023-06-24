d19e1255a293629bbc3f313f8c9dbbb945501969
Vanderson <vandimavellar1997@gmail.com>
2023-06-24 01:20:08 -0300

    implements auth0

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import { AppProps } from "next/app";
+import {UserProvider} from '@auth0/nextjs-auth0/client'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />)
+    </UserProvider>
+  )
+}
